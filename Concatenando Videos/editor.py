6fc2c7159b65bb3b4df5a809310ea34f6fdf6421
dhtrgabriell <dhtrgabriell@outlook.com>
2024-03-11 16:06:19 -0300

    add

--- Concatenando Videos/editor.py
+++ Concatenando Videos/editor.py
@@ -1,2 +1,13 @@
-from moviepy.editor import VideoFileClip, concatenate_videoclip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
+clipe1 = VideoFileClip("clip1.mp4")
+clipe2 = VideoFileClip("clip2.mp4")
+clipe3 = VideoFileClip("clip3.mp4")
+
+video_completo = concatenate_videoclips([
+    clipe1,
+    clipe2,
+    clipe3
+])
+
+video_completo.write_videofile('VideoCompleto.mp4')
